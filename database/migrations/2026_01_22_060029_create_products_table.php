4830675143e9b2f95f89552024f81c7e83bb438f
Fdhlnnn <fadhlan.aghnatan05@gmail.com>
2026-01-22 14:13:58 +0700

    feat: Update products and events migrations

--- database/migrations/2026_01_22_060029_create_products_table.php
+++ database/migrations/2026_01_22_060029_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price');
+            $table->text('content')->nullable();
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
