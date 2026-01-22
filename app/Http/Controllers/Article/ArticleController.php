4a4cffcab50bcd0bda3da02dd55438f472f1413a
Fdhlnnn <fadhlan.aghnatan05@gmail.com>
2026-01-22 13:44:43 +0700

    feat: Add function index,create in ArticleController

--- app/Http/Controllers/Article/ArticleController.php
+++ app/Http/Controllers/Article/ArticleController.php
@@ -1,10 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Inertia\Inertia;
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    //
+    public function index()
+    {
+        return inertia::render('Articles/Index');
+    }
+
+    public function create()
+    {
+        return inertia::render('Articles/Create', [
+            'title' => 'Create Article Title',
+            'content' => 'Create Article Content',
+            'thumbnail' => 'Upload Article Thumbnail',
+            'author' => 'Insert Author Name',
+        ]);
+    }
+
 }
